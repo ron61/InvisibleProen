ed17167feb14fef1c1b88c0e57aa8f0237658716
Ryu Kudoh <ron233f@gmail.com>
2019-07-10 15:53:09 +0900

    透明になった．移動できるようになった．大きさか変になった．

---
+++ Unity-Technologies-unity-arkit-plugin-94e47eae5954/Assets/Scripts/ToggleUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleUI : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject plusxButton;
+    [SerializeField]
+    GameObject minusxButton;
+    [SerializeField]
+    GameObject plusyButton;
+    [SerializeField]
+    GameObject minusyButton;
+    [SerializeField]
+    GameObject pluszButton;
+    [SerializeField]
+    GameObject minuszButton;
+    [SerializeField]
+    GameObject bigButton;
+    [SerializeField]
+    GameObject smallButton;
+
+    bool isActivated = true;
+
+    /// ボタンをクリックした時の処理
+    public void OnClick()
+    {
+        if(isActivated)
+        {
+            plusxButton.SetActive(false);
+            minusxButton.SetActive(false);
+            plusyButton.SetActive(false);
+            minusyButton.SetActive(false);
+            pluszButton.SetActive(false);
+            minuszButton.SetActive(false);
+            bigButton.SetActive(false);
+            smallButton.SetActive(false);
+        }
+        else
+        {
+            plusxButton.SetActive(true);
+            minusxButton.SetActive(true);
+            plusyButton.SetActive(true);
+            minusyButton.SetActive(true);
+            pluszButton.SetActive(true);
+            minuszButton.SetActive(true);
+            bigButton.SetActive(true);
+            smallButton.SetActive(true);
+        }
+        isActivated = !isActivated;
+    }
+}
+
+
+
---
+++ Unity-Technologies-unity-arkit-plugin-94e47eae5954/Assets/Scripts/smallButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smallButton : MonoBehaviour
+{
+    [SerializeField]
+    GameObject manager;
+    GenerateImageAnchor generateImageAnchor;
+    float x, y, z;
+
+    private void Start()
+    {
+        generateImageAnchor = manager.GetComponent<GenerateImageAnchor>();
+    }
+
+    public void OnClick()
+    {
+        generateImageAnchor.shinkawaScale *= 0.95f;
+    }
+}
